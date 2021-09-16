360e5bd913a49bffa3678ce9f89550b710bca665
lovetyagi-17 <ltyagi58@gmail.com>
2021-09-16 18:21:34 +0530

    Subtract the Product and Sum of Digits of an Integer.

--- Conditional _and_Loop/practice_loops/productandsum.java
+++ Conditional _and_Loop/practice_loops/productandsum.java
@@ -6,6 +6,19 @@ public class productandsum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        
+        System.out.print("Enter no.: ");
+        int num = in.nextInt();
+        int multiplyResult = 1;
+        int sumResult = 0;
+        while(num > 0) {
+            int temp = num % 10;
+            num /= 10;
+            multiplyResult *= temp; 
+            sumResult += temp;
+        }
+        // System.out.println("Multiply result is: " + multiplyResult);
+        // System.out.println("SUM result is: " + sumResult);
+        System.out.println(multiplyResult - sumResult);
+
     }
 }
