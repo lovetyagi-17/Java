0e763474c1c6dabce89c8f531e50ae8e75b0abfe
lovetyagi-17 <ltyagi58@gmail.com>
2021-09-16 15:13:52 +0530

    setup for loop practice

---
+++ Conditional _and_Loop/practice_loops/productandsum.java
@@ -0,0 +1,11 @@
+package practice_loops;
+import java.util.Scanner;
+
+//Subtract the Product and Sum of Digits of an Integer
+public class productandsum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        
+    }
+}
