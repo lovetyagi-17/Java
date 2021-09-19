3a07c06750fbae7497be0185d737a206aad281a4
lovetyagi-17 <ltyagi58@gmail.com>
2021-09-19 14:55:45 +0530

    swap code setup.

---
+++ Methods/swap.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        int response =swap(10, 12);
+        System.out.println(response);
+    }
+
+
+    static int swap(int x, int y) {
+        int result = 0;
+        return result;
+    }
+}
